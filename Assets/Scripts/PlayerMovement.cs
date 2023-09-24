d7b0b72a6219557c7bf11e5ecefa6b026d02a2a8
kelethe <kelethe@outlook.com>
2023-09-25 02:44:36 +0300

    Fixed running animation
    Fixed running animation and its script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movespeed;
+    [SerializeField] private float walkspeed;
     [SerializeField] private float runspeed;
     [SerializeField] private float jumpspeed;
     private Rigidbody2D body;
     private Animator animator;
-    private bool isRunning;
     private bool isGrounded;
      
     private void Awake()
@@ -27,21 +27,46 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // Walk movement
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * movespeed, body.velocity.y);
 
-        // Run movement
-        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        // Run & walk movement
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            Run();
+            if (horizontalInput != 0)
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isRunning", true);
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isRunning", false);
+            }
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * runspeed, body.velocity.y);
         }
-
+        else
+        {
+            if (horizontalInput != 0)
+            {
+                animator.SetBool("isWalking", true);
+                animator.SetBool("isRunning", false);
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetBool("isRunning", false);
+            }
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * walkspeed, body.velocity.y);
+        }
+            
         // Jump movement
         if(Input.GetKey(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
+
+        // Set animator parameters
+        animator.SetBool("isGrounded", isGrounded);
 
         // Flip player when moving left-right
         if (horizontalInput > 0.01f)
@@ -52,18 +77,6 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-
-        // Set animator parameters
-        animator.SetBool("isWalking", horizontalInput != 0);
-        animator.SetBool("isRunning", !isRunning);
-        animator.SetBool("isGrounded", isGrounded);
-    }
-
-    private void Run()
-    {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * runspeed, body.velocity.y);
-        animator.SetTrigger("Run");
-        isRunning = true;
     }
 
     private void Jump()
