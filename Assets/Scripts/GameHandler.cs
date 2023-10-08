090d8e7969b16175c754495c8498204c3a0b4693
Qihui Zhang <chris.qh.zhang@outlook.com>
2023-10-08 13:01:30 +0300

    Set up health system and health bar with sprites, scripts and animation

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandller : MonoBehaviour
+{
+    public Transform objHealthBar;
+    public Transform Player;
+    public Transform healthBarSpawnPoint;
+    //public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);        
+
+        // gameobject objHealthBar.SetActive
+        objHealthBar.transform.gameObject.SetActive(false);
+        Transform healthBarTransform = Instantiate(objHealthBar, new Vector3(-7.39f, 5.2f, 0), Quaternion.identity, Player.transform);
+        // position constraint component
+        // objHealthBar.transform.SetParent(Player, false);
+        // objHealthBar.transform.SetPositionAndRotation(new Vector3(0, 1.2f, 0), Quaternion.identity);
+
+        HealthBar healthBar = healthBarTransform.GetComponent<HealthBar>();
+        healthBar.Setup(healthSystem);
+
+        Debug.Log("Health:" + healthSystem.GetHealth());
+
+
+    }
+
+
+}
